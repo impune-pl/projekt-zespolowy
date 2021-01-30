f4782160032bf8a08d2d1146ee8297223e627885
therobby <40268355+therobby@users.noreply.github.com>
2021-01-30 19:31:36 +0100

    Add files via upload

---
+++ pz-backend/src/objects/detiledcontact.ts
@@ -0,0 +1,15 @@
+import User from "./user";
+
+// Data class
+export default class DetiledContact {
+	public id: number;
+	public userId: number;
+	public contact: User;
+	public isBlocked: boolean;
+	public isLocationShared: boolean;
+	public isAccepted: boolean;
+
+	// constructor() {
+	// 	//this.id = 0;
+	// }
+}
