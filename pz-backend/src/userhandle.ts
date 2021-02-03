ad561504ca1598c04e05cec27bc5a94ca35f18f6
therobby <40268355+therobby@users.noreply.github.com>
2021-02-03 20:48:55 +0100

    Fixed always returning true on accept contact

--- pz-backend/src/userhandle.ts
+++ pz-backend/src/userhandle.ts
@@ -192,8 +192,8 @@ export default class UserHandle {
 		return new Promise((resolve, reject) => {
 			this.db.crud.contacts
 				.accept(contact_id)
-				.then(() => {
-					resolve(true);
+				.then((res: pg.QueryResult) => {
+					resolve(res.rowCount == 1);
 				})
 				.catch((err) => {
 					console.error({ accept_invitation: err });
@@ -206,8 +206,8 @@ export default class UserHandle {
 		return new Promise((resolve, reject) => {
 			this.db.crud.contacts
 				.dismiss(contact_id)
-				.then(() => {
-					resolve(true);
+				.then((res: pg.QueryResult) => {
+					resolve(res.rowCount == 1);
 				})
 				.catch((err) => {
 					console.error({ dismiss_invitation: err });
